14ce5d8cdf97834d60bca419804cb4cc1fb79600
srapatnetwork <srapatnetwork@gmail.com>
2025-06-10 07:00:13 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,26 +1,34 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(req) {
-  const basicAuth = req.headers.get('authorization')
   const url = req.nextUrl
+  const isProtected = url.pathname.startsWith('/generate') || url.pathname.startsWith('/admin')
 
-  if (url.pathname.startsWith('/generate')) {
-    if (!basicAuth) {
-      return new NextResponse('Auth Required', {
-        status: 401,
-        headers: { 'WWW-Authenticate': 'Basic realm="Admin Area"' }
-      })
-    }
+  if (!isProtected) return NextResponse.next()
 
+  const basicAuth = req.headers.get('authorization')
+
+  if (!basicAuth) {
+    return new NextResponse('Auth Required', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Admin Area"'
+      }
+    })
+  }
+
+  try {
     const authValue = basicAuth.split(' ')[1]
     const [user, pwd] = atob(authValue).split(':')
 
-    if (
-      user !== process.env.ADMIN_USERNAME ||
-      pwd !== process.env.ADMIN_PASSWORD
-    ) {
+    const username = process.env.ADMIN_USERNAME
+    const password = process.env.ADMIN_PASSWORD
+
+    if (user !== username || pwd !== password) {
       return new NextResponse('Unauthorized', { status: 403 })
     }
+  } catch {
+    return new NextResponse('Invalid Authorization Header', { status: 400 })
   }
 
   return NextResponse.next()
